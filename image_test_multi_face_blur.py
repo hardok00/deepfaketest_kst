00658f13b101bd576593a8c955c541d1635d1d9e
hardok00 <65234331+hardok00@users.noreply.github.com>
2023-04-03 11:36:36 +0900

    Update image_test_multi_face_blur.py

--- image_test_multi_face_blur.py
+++ image_test_multi_face_blur.py
@@ -10,7 +10,7 @@ from models.model import FaceSwap, l2_norm
 from models.arcface import IRBlock, ResNet
 from utils.align_face import back_matrix, dealign, align_img
 from utils.util import paddle2cv, cv2paddle
-from utils.prepare_data_verify import LandmarkModel
+from utils.prepare_data import LandmarkModel
 
 def draw_text(img, text,
           font=cv2.FONT_HERSHEY_PLAIN,
@@ -29,32 +29,7 @@ def draw_text(img, text,
 
     return text_size
 
-def get_id_emb(id_net, id_img_path):
-    id_img = cv2.imread(id_img_path)
-
-    id_img = cv2.resize(id_img, (112, 112))
-    id_img = cv2paddle(id_img)
-    mean = paddle.to_tensor([[0.485, 0.456, 0.406]]).reshape((1, 3, 1, 1))
-    std = paddle.to_tensor([[0.229, 0.224, 0.225]]).reshape((1, 3, 1, 1))
-    id_img = (id_img - mean) / std
-
-    id_emb, id_feature = id_net(id_img)
-    id_emb = l2_norm(id_emb)
-
-    return id_emb, id_feature
-
-def get_id_emb_from_image(id_net, id_img):
-    id_img = cv2.resize(id_img, (112, 112))
-    id_img = cv2paddle(id_img)
-    mean = paddle.to_tensor([[0.485, 0.456, 0.406]]).reshape((1, 3, 1, 1))
-    std = paddle.to_tensor([[0.229, 0.224, 0.225]]).reshape((1, 3, 1, 1))
-    id_img = (id_img - mean) / std
-    id_emb, id_feature = id_net(id_img)
-    id_emb = l2_norm(id_emb)
-
-    return id_emb, id_feature
-
-def image_test_multi_face(args, source_aligned_images, target_aligned_images, bboxes):
+def image_test_multi_face(args, bboxes):
     paddle.set_device("gpu" if args.use_gpu else 'cpu')
     faceswap_model = FaceSwap(args.use_gpu)
 
@@ -71,66 +46,18 @@ def image_test_multi_face(args, source_aligned_images, target_aligned_images, bb
     else:
         target_name = args.target_img_path
     origin_att_img = cv2.imread(args.target_img_path)
-    
-    print(len(target_aligned_images))
-
-    for idx, target_aligned_image in enumerate(target_aligned_images):
-        id_emb, id_feature = get_id_emb_from_image(id_net, source_aligned_images[idx % len(source_aligned_images)][0])
-        faceswap_model.set_model_param(id_emb, id_feature, model_weight=weight)
-        faceswap_model.eval()
-
-        att_img = cv2paddle(target_aligned_image[0])
-
-        res, mask = faceswap_model(att_img)
-        res = paddle2cv(res)
-
-        back_matrix = target_aligned_images[idx % len(target_aligned_images)][1]
-        mask = np.transpose(mask[0].numpy(), (1, 2, 0))
-        origin_att_img = dealign(res, origin_att_img, back_matrix, mask)
         
-    for bbox in bboxes:
+    for idx, bbox in enumerate(bboxes):
         if bbox[1] < 0:
             bbox[1] = 0
         # print(bbox)
         p1, p2 = (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3]))
         origin_att_img[p1[1]: p2[1], p1[0]:p2[0], :] = cv2.GaussianBlur(origin_att_img[p1[1]: p2[1], p1[0]:p2[0], :], (0,0), 20 )
-        draw_text(origin_att_img, str(bbox[4]), pos=(int(bbox[0]), int(bbox[1])), font_scale=1, font_thickness= 1, text_color=(255, 255, 255), text_color_bg=(0, 0, 255))
+        # draw_text(origin_att_img, str(bbox[4]), pos=(int(bbox[0]), int(bbox[1])), font_scale=1, font_thickness= 1, text_color=(255, 255, 255), text_color_bg=(0, 0, 255))
     
     cv2.imwrite(os.path.join(args.output_dir, os.path.basename(target_name).format(idx)), origin_att_img)
 
-# 소스이미지 등록 및 추출, 비교이미지 랜드마크 추출
-def source_face_align(landmarkModel, source_image_path, verify_image_path, image_size=224):
-    source_aligned_imgs =[]
-    if os.path.isfile(source_image_path):
-        img_list = [source_image_path]
-    else:
-        img_list = [os.path.join(source_image_path, x) for x in os.listdir(source_image_path) if x.endswith('png') or x.endswith('jpg') or x.endswith('jpeg')]
-    for path in img_list:
-        img = cv2.imread(path)
-        landmark = landmarkModel.get(img)
-        if landmark is not None:
-            # base_path = path.replace('.png', '').replace('.jpg', '').replace('.jpeg', '')
-            aligned_img, back_matrix = align_img(img, landmark, image_size)
-            source_aligned_imgs.append([aligned_img, back_matrix])
-
-            # cv2.imwrite(base_path + '_aligned.png', aligned_img)
-            # np.save(base_path + '_back.npy', back_matrix)
-            
-        else:
-            print("*************source face no detect***************")
-            
-    verify_landmark =[]
-    verify_list = [os.path.join(verify_image_path, x) for x in os.listdir(verify_image_path) if x.endswith('png') or x.endswith('jpg') or x.endswith('jpeg')]
-    print(verify_list)
-    for path in verify_list:
-        verify_img = cv2.imread(path)
-        landmark = landmarkModel.get(verify_img)
-        
-        verify_landmark.append(landmark)
-            
-    return source_aligned_imgs, verify_landmark, verify_list
-
-def target_faces_align(landmarkModel, target_image_path, verify_landmark, verify_list, image_size=224):
+def target_faces_align(landmarkModel, target_image_path, image_size=224):
     aligned_imgs =[]
 
     if os.path.isfile(target_image_path):
@@ -138,41 +65,16 @@ def target_faces_align(landmarkModel, target_image_path, verify_landmark, verify
     else:
         img_list = [os.path.join(target_image_path, x) for x in os.listdir(target_image_path) if x.endswith('png') or x.endswith('jpg') or x.endswith('jpeg')]
     for path in img_list:
-        # verify_img = cv2.imread(verify_image_path)
         target_img = cv2.imread(path)
-        name = path.split('/')[-1].replace('.png', '').replace('.jpg', '').replace('.jpeg', '')
-
-        target_path = f"asset/{name}_target"
-        os.makedirs(target_path, exist_ok=True)
         
-        bboxes, landmarks = landmarkModel.gets(target_img, verify_landmark, verify_list)
-        
-        # print(landmarks)
-        
-        for target_count, landmark in enumerate(landmarks):
-            if landmark is not None:
-                aligned_img, back_matrix = align_img(target_img, landmark, image_size)
-                aligned_imgs.append([aligned_img, back_matrix])
-                                            
-                # 타겟 이미지 여러개 추출
-                cv2.imwrite(target_path + f'/{name}_target_{target_count}.png', aligned_img)
-                np.save(target_path + f'/{name}_target_back_{target_count}.npy', back_matrix)
-            else:
-                print("*************face no detect***************")
-                
-
-        target_list = [os.path.join(target_path, x) for x in os.listdir(target_path) if x.endswith('png') or x.endswith('jpg') or x.endswith('jpeg')]
-        
-        print(target_list) 
-
+        bboxes, landmarks = landmarkModel.gets(target_img)
+            
     return bboxes, aligned_imgs
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="MobileFaceSwap Test")
-    parser.add_argument('--verify_img_path', type=str, help='path to the source image')
-    parser.add_argument('--source_img_path', type=str, help='path to the source image')
     parser.add_argument('--target_img_path', type=str, help='path to the target images')
     parser.add_argument('--output_dir', type=str, default='results', help='path to the output dirs')
     parser.add_argument('--image_size', type=int, default=224,help='size of the test images (224 SimSwap | 256 FaceShifter)')
@@ -185,10 +87,9 @@ if __name__ == '__main__':
     if args.need_align:
         landmarkModel = LandmarkModel(name='landmarks')
         landmarkModel.prepare(ctx_id= 0, det_thresh=0.6, det_size=(640,640))
-        source_aligned_images,verify_landmark,verify_list = source_face_align(landmarkModel, args.source_img_path, args.verify_img_path)
-        bboxes, target_aligned_images = target_faces_align(landmarkModel, args.target_img_path, verify_landmark,verify_list, args.image_size)
+        bboxes, target_aligned_images = target_faces_align(landmarkModel, args.target_img_path, args.image_size)
     os.makedirs(args.output_dir, exist_ok=True)
-    image_test_multi_face(args, source_aligned_images, target_aligned_images, bboxes)
+    image_test_multi_face(args, bboxes)
 
 
 

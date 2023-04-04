d4f47d603c05b46284460bf489629dff31166006
hardok00 <65234331+hardok00@users.noreply.github.com>
2023-04-04 16:16:36 +0900

    Update image_test_multi_face_blur.py

--- image_test_multi_face_blur.py
+++ image_test_multi_face_blur.py
@@ -10,7 +10,8 @@ from models.model import FaceSwap, l2_norm
 from models.arcface import IRBlock, ResNet
 from utils.align_face import back_matrix, dealign, align_img
 from utils.util import paddle2cv, cv2paddle
-from utils.prepare_data import LandmarkModel
+from utils.prepare_data_verify import LandmarkModel
+import json
 
 def draw_text(img, text,
           font=cv2.FONT_HERSHEY_PLAIN,
@@ -29,53 +30,72 @@ def draw_text(img, text,
 
     return text_size
 
-def image_test_multi_face(args, bboxes):
-    paddle.set_device("gpu" if args.use_gpu else 'cpu')
-    faceswap_model = FaceSwap(args.use_gpu)
-
-    id_net = ResNet(block=IRBlock, layers=[3, 4, 23, 3])
-    id_net.set_dict(paddle.load('./checkpoints/arcface.pdparams'))
-
-    id_net.eval()
-
-    weight = paddle.load('./checkpoints/MobileFaceSwap_224.pdparams')
-
-    start_idx = args.target_img_path.rfind('/')
-    if start_idx > 0:
-        target_name = args.target_img_path[args.target_img_path.rfind('/'):]
+def image_test_multi_face(args, landmarkModel):
+    if os.path.isfile(args.json_path):
+        with open(args.json_path, 'rt', encoding='UTF-8') as annotations:
+            coco = json.load(annotations)
     else:
-        target_name = args.target_img_path
-    origin_att_img = cv2.imread(args.target_img_path)
-        
-    for idx, bbox in enumerate(bboxes):
-        if bbox[1] < 0:
-            bbox[1] = 0
-        # print(bbox)
-        p1, p2 = (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3]))
-        origin_att_img[p1[1]: p2[1], p1[0]:p2[0], :] = cv2.GaussianBlur(origin_att_img[p1[1]: p2[1], p1[0]:p2[0], :], (0,0), 20 )
-        # draw_text(origin_att_img, str(bbox[4]), pos=(int(bbox[0]), int(bbox[1])), font_scale=1, font_thickness= 1, text_color=(255, 255, 255), text_color_bg=(0, 0, 255))
+        print("********************* JSON FILE NOT DETECT *****************************")
+        return None
     
-    cv2.imwrite(os.path.join(args.output_dir, os.path.basename(target_name).format(idx)), origin_att_img)
-
-def target_faces_align(landmarkModel, target_image_path, image_size=224):
-    aligned_imgs =[]
-
-    if os.path.isfile(target_image_path):
-        img_list = [target_image_path]
+    if os.path.isfile(args.target_img_path):
+        img_list = [args.target_img_path]
     else:
-        img_list = [os.path.join(target_image_path, x) for x in os.listdir(target_image_path) if x.endswith('png') or x.endswith('jpg') or x.endswith('jpeg')]
-    for path in img_list:
-        target_img = cv2.imread(path)
+        img_list = [os.path.join(args.target_img_path, x) for x in os.listdir(args.target_img_path) if x.endswith('png') or x.endswith('jpg') or x.endswith('jpeg')]
+    img_list.sort()
+    
+    verify_landmark =[]
+    verify_list = [os.path.join(args.verify_img_path, x) for x in os.listdir(args.verify_img_path) if x.endswith('png') or x.endswith('jpg') or x.endswith('jpeg')]
+    print(verify_list)
+    for path in verify_list:
+        verify_img = cv2.imread(path)
+        landmark = landmarkModel.get(verify_img)
         
-        bboxes, landmarks = landmarkModel.gets(target_img)
-            
-    return bboxes, aligned_imgs
+        verify_landmark.append(landmark)
+    
+    for idx, path in enumerate(img_list):
+        start_idx = path.rfind('/')
+        if start_idx > 0:
+            target_name = path[path.rfind('/'):]
+        else:
+            target_name = args.target_img_path
 
+        origin_att_img = cv2.imread(path)
+        bboxes = []
+
+        for image_id in coco['annotations']:
+            if image_id["image_id"] == idx + 1 and image_id["category_id"] == 2:
+                image_box = image_id["bbox"]
+                image_box = list(map(int, image_box))
+                # print(image_box)
+                cropped_image = origin_att_img[image_box[1]:image_box[1]+image_box[3], image_box[0]:image_box[0]+image_box[2]]
+                
+                bbox, landmarks = landmarkModel.gets(cropped_image,verify_landmark,verify_list)
+                # print(f"crop bbox : {bbox}" )
+                
+                for j in bbox:
+                    j = list(map(int, j))
+                    del j[4]
+                    # print(j)
+                    bboxes.append([image_box[0]+j[0],image_box[1]+j[1],image_box[0]+j[2],image_box[1]+j[3]])
+                # print(bboxes)
+                # cv2.imwrite(os.path.join(args.output_dir, os.path.basename(target_name)), cropped_image)
+        
+        for idx, bbox in enumerate(bboxes):
+            # print(bbox)
+            if bbox[1] < 0:
+                bbox[1] = 0
+            p1, p2 = (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3]))
+            origin_att_img[p1[1]: p2[1], p1[0]:p2[0], :] = cv2.GaussianBlur(origin_att_img[p1[1]: p2[1], p1[0]:p2[0], :], (0,0), 20 )
+            # draw_text(origin_att_img, str(bbox[4]), pos=(int(bbox[0]), int(bbox[1])), font_scale=1, font_thickness= 1, text_color=(255, 255, 255), text_color_bg=(0, 0, 255))
+        
+        cv2.imwrite(os.path.join(args.output_dir, os.path.basename(target_name).format(idx)), origin_att_img)
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description="MobileFaceSwap Test")
+    parser.add_argument('--verify_img_path', type=str, help='path to the verify image')
     parser.add_argument('--target_img_path', type=str, help='path to the target images')
+    parser.add_argument('--json_path', type=str, help='path to the json')
     parser.add_argument('--output_dir', type=str, default='results', help='path to the output dirs')
     parser.add_argument('--image_size', type=int, default=224,help='size of the test images (224 SimSwap | 256 FaceShifter)')
     parser.add_argument('--merge_result', type=bool, default=True, help='output with whole image')
@@ -87,9 +107,8 @@ if __name__ == '__main__':
     if args.need_align:
         landmarkModel = LandmarkModel(name='landmarks')
         landmarkModel.prepare(ctx_id= 0, det_thresh=0.6, det_size=(640,640))
-        bboxes, target_aligned_images = target_faces_align(landmarkModel, args.target_img_path, args.image_size)
     os.makedirs(args.output_dir, exist_ok=True)
-    image_test_multi_face(args, bboxes)
+    image_test_multi_face(args, landmarkModel)
 
 
 

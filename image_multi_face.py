85f6e4c6a438418c608e2f4e1f7e0fe2864906ab
hardok00 <65234331+hardok00@users.noreply.github.com>
2023-04-06 12:52:58 +0900

    이미지 얼굴 딥페이크 다수 대상 처리

--- image_multi_face.py
+++ image_multi_face.py
@@ -151,3 +151,5 @@ if __name__ == '__main__':
         bboxes, target_aligned_images = faces_align(landmarkModel, args.target_img_path, args.image_size)
     os.makedirs(args.output_dir, exist_ok=True)
     image_test_multi_face(args, source_aligned_images, target_aligned_images, bboxes)
+
+          

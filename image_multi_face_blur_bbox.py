15808507d8a05dc401c2ae3cff79959ab45cae30
hardok00 <65234331+hardok00@users.noreply.github.com>
2023-04-06 12:47:37 +0900

    이미지 얼굴 BBOX 블러 처리

--- image_multi_face_blur_bbox.py
+++ image_multi_face_blur_bbox.py
@@ -89,15 +89,3 @@ if __name__ == '__main__':
         bboxes = target_faces_align(landmarkModel, args.target_img_path, args.json_path, args.image_size)
     os.makedirs(args.output_dir, exist_ok=True)
     image_test_multi_face(args, bboxes)
-
-
-
-
-
-
-
-
-
-
-
-
